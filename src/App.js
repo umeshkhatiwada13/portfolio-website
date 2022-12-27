4bda926f183c882e46f85af44435234f134ecbd4
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2022-12-27 16:10:33 +0545

    addition of favicon

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Tools from "./components/Tools";
 import Experience from "./components/Experience";
-import Education from "./components/Education";
+// import Education from "./components/Education";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
